aadbdbdfe0679c17c94b17f4679668f9c87d730c
laybelmeyden <trycollens@gmail.com>
2021-03-13 16:37:22 +0300

    deleted.mail

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -154,7 +154,7 @@ class ProjectController extends Controller
         $project->statuses = 'moderate';
         $project->save();
 
-        return back();
+        return redirect('/home')->back();
     }
     public function updateProject(Request $request, Project $project)
     {
