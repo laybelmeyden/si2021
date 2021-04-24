85897178bd9e597bbf743ef666a2c258183e083c
laybelmeyden <trycollens@gmail.com>
2021-04-24 17:41:24 +0300

    added/online-courses

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,9 @@
     <title>@yield('title')</title>
     <link rel="shortcut icon" href="{{asset('assets/img/favicon.ico')}}" type="image/x-icon">
 
+    <meta property="og:image" content="{{asset('assets/img/years/1444000.png')}}">
+    <meta property="og:url" content="{{asset('assets/img/years/1444000.png')}}">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     @include('layouts.css')
@@ -36,12 +39,12 @@
     <!-- /Yandex.Metrika counter -->
     <script>
         const msg = 'Social Idea 2021';
-        const msgTitle = (icon, text) =>{
+        const msgTitle = (icon, text) => {
             document.querySelector('head title').textContent = text;
             document.querySelector('link[rel="shortcut icon"]').setAttribute('href', icon);
         }
-        window.onblur = () =>{
-            setTimeout(()=>{
+        window.onblur = () => {
+            setTimeout(() => {
                 msgTitle('/assets/img/heart.png', 'Возвращайтесь, мы вас ждем !')
             }, 1000)
         }
